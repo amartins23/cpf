d92e278e535dd0f41f3d6d5af218f32fe4ac55cc
amartins23 <andre.martins@hitachivantara.com>
2018-07-25 11:33:46 +0100

    Implement properties update on saveFile for repository

--- rca/src/main/java/org/pentaho/ctools/cpf/repository/rca/RemoteUserContentAccess.java
+++ rca/src/main/java/org/pentaho/ctools/cpf/repository/rca/RemoteUserContentAccess.java
@@ -14,11 +14,18 @@ package org.pentaho.ctools.cpf.repository.rca;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.pentaho.ctools.cpf.repository.rca.dto.StringKeyStringValueDto;
 import pt.webdetails.cpf.api.IFileContent;
 import pt.webdetails.cpf.api.IUserContentAccessExtended;
 import pt.webdetails.cpf.repository.api.FileAccess;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RemoteUserContentAccess extends RemoteReadWriteAccess implements IUserContentAccessExtended {
 
@@ -38,10 +45,33 @@ public class RemoteUserContentAccess extends RemoteReadWriteAccess implements IU
   public boolean saveFile( IFileContent file ) {
     //TODO: dummy implementation
     try {
-      return saveFile( file.getPath(), file.getContents() );
+      if ( saveFile( file.getPath(), file.getContents() ) )
+      {
+        return updateProperties( file );
+      }
     } catch ( IOException ex ) {
       logger.error( ex );
-      return false;
     }
+    return false;
+  }
+
+  private boolean updateProperties( IFileContent file ) {
+    final String defaultLocale = "default"; // use default locale
+    List<StringKeyStringValueDto> properties = new ArrayList<>();
+    properties.add( new StringKeyStringValueDto( "file.title", file.getTitle() ) );
+    properties.add( new StringKeyStringValueDto( "file.description", file.getDescription() ) );
+
+    GenericEntity<List<StringKeyStringValueDto>> entity = new GenericEntity<List<StringKeyStringValueDto>>( properties )
+    {
+    };
+
+    String requestURL = createRequestURL( "/api/repo/files/", file.getPath(), "localeProperties" );
+    Response response = client.target( requestURL )
+      .queryParam( "locale", defaultLocale)
+      .request( MediaType.APPLICATION_XML )
+      .put( Entity.xml( entity ) );
+
+    //TODO: handle non-OK status codes? log? exception?
+    return response.getStatus() == Response.Status.OK.getStatusCode();
   }
 }
