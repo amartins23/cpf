567553a15c815511b7414483344c9c8cd5fff2d0
amartins23 <andre.martins@hitachivantara.com>
2018-07-30 10:21:56 +0100

    Update documentation and clean up code

--- osgi/src/main/java/org/pentaho/ctools/cpf/repository/factory/ContentAccessFactory.java
+++ osgi/src/main/java/org/pentaho/ctools/cpf/repository/factory/ContentAccessFactory.java
@@ -35,18 +35,16 @@ import pt.webdetails.cpf.repository.api.IRWAccess;
 
 
 /**
- * The {@code ContentAccessFactory} class creates repository access providers for basic plugin needs.
- * These access providers are instances of {@code ReadAccessProxy} that contain a reference to an internal dynamic list
- * of available {@code IReadAccess} services that allow access to the available resources.
+ * The {@code ContentAccessFactory} class creates access providers for user content {@code IUserContentAccessExtended}
+ * and plugin content ({@code IReadAccess} or {@code IRWAccess}) for OSGi environments.
  *
- * Additionally, user content {@code IUserContentAccess} can also use a single dynamic instance of a {@code IRWAccess}
- * service to provide write operations.
+ * User content is backed by a supplied instance of {@code IUserContentAccessExtended} and plugin content is backed
+ * by the filesystem. Plugin content has separate namespaces (folders) for plugin-system (/system/&lt;plugin-id&gt;/)
+ * and plugin-repos (/repos/), with the plugin-system segregated by plugin-id.
  *
- * Note: To facilitate operations by CDE Editor, a dummy instance is returned from {@code getPluginSystemWriter} and
- * {@code getOtherPluginSystemWriter} that fakes write operations and forwards read operations to an instance of
- * {@code ReadAccessProxy}.
- *
- * Note: PluginRepository write access is currently not supported.
+ * Both user content and plugin-system content can be augmented by supplying instances of {@code IReadAccess} with
+ * appropriate properties defined: (isUserContent = true) for user content and (pluginId) for plugin content. These are
+ * combined with the base providers, supplying read-only content for paths not found in the base providers.
  *
  * @see IContentAccessFactoryExtended
  * @see IUserContentAccessExtended
@@ -56,21 +54,40 @@ import pt.webdetails.cpf.repository.api.IRWAccess;
 public final class ContentAccessFactory implements IContentAccessFactoryExtended {
   private static final Log logger = LogFactory.getLog( ContentAccessFactory.class );
   private static final String SERVICE_PROPERTY_PLUGIN_ID = "pluginId";
+  private static final String SERVICE_PROPERTY_IS_USER_CONTENT = "isUserContent";
   private static final String PLUGIN_REPOS_NAMESPACE = "repos";
   private static final String PLUGIN_SYSTEM_NAMESPACE = "system";
   private Map<String, List<IReadAccess>> pluginReadAccessMap = new HashMap<>();
   private List<IReadAccess> userContentReadAccesses = new ArrayList<>();
   private IUserContentAccessExtended userContentAccess = null;
-  private final String volumePath;
+  private String baseStoragePath;
   private final String parentPluginId;
   private FileSystem storageFilesystem = FileSystems.getDefault();
+
+  /**
+   * @param parentPluginId is the ID of the CTools plugin using this instance.
+   *
+   */
+  public ContentAccessFactory( String parentPluginId ) {
+    this.baseStoragePath = System.getProperty( "java.io.tmpdir" );
+    this.parentPluginId = parentPluginId;
+  }
+
+  /**
+   * @param baseStoragePath is the base path for plugin namespaces on the backing filesystem.
+   * @param parentPluginId is the ID of the CTools plugin using this instance.
+   */
+  public ContentAccessFactory( String baseStoragePath, String parentPluginId ) {
+    this.baseStoragePath = baseStoragePath;
+    this.parentPluginId = parentPluginId;
+  }
 
   /**
    * Add new read-only content to the plugin system namespace.
    * @param pluginId identifier of the plugin
    * @param readAccess instance of read access to the plugin resources
    */
-  public void addPluginSystemAccess( String pluginId, IReadAccess readAccess ) {
+  public void addReadOnlyPluginSystemAccess( String pluginId, IReadAccess readAccess ) {
     List<IReadAccess> pluginList = getPluginSystemAccessList( pluginId );
     pluginList.add( readAccess );
   }
@@ -89,31 +106,57 @@ public final class ContentAccessFactory implements IContentAccessFactoryExtended
   }
 
   /**
-   * Remove read-only content to the plugin system namespace.
+   * Remove read-only content from the plugin system namespace.
    * @param pluginId identifier of the plugin
    * @param readAccess instance of read access to the plugin resources
    */
-  public void removePluginSystemAccess( String pluginId, IReadAccess readAccess ) {
+  public void removeReadOnlyPluginSystemAccess( String pluginId, IReadAccess readAccess ) {
     List<IReadAccess> pluginList = this.pluginReadAccessMap.get( pluginId );
     if ( pluginList != null ) {
       pluginList.remove( readAccess );
     }
   }
 
+  /**
+   * Add read-only user content or plugin-system resource access providers.
+   *
+   * User content must have a boolean "isUserContent" property defined and set to true.
+   * Plugin-system resources must have a string "pluginId" property defined.
+   * @param readAccess is the read-only content provider to be added.
+   * @param serviceProperties is the map of service properties for the supplied content provider.
+   */
   public void addReadAccess( IReadAccess readAccess, Map serviceProperties ) {
     if ( !serviceProperties.isEmpty() ) {
       Object id = serviceProperties.get( SERVICE_PROPERTY_PLUGIN_ID );
       if ( id != null && id instanceof String ) {
-        addPluginSystemAccess( (String) id, readAccess );
+        addReadOnlyPluginSystemAccess( (String) id, readAccess );
+      } else {
+        Object isUserContent = serviceProperties.get( SERVICE_PROPERTY_IS_USER_CONTENT );
+        if ( isUserContent != null && isUserContent instanceof Boolean && ((Boolean) isUserContent).booleanValue() ) {
+          userContentReadAccesses.add( readAccess );
+        }
       }
     }
   }
 
+  /**
+   * Remove read-only user content or plugin-system resource access providers.
+   *
+   * User content must have a boolean "isUserContent" property defined and set to true.
+   * Plugin-system resources must have a string "pluginId" property defined.
+   * @param readAccess is the read-only content provider to be removed.
+   * @param serviceProperties is the map of service properties for the supplied content provider.
+   */
   public void removeReadAccess( IReadAccess readAccess, Map serviceProperties ) {
     if ( !serviceProperties.isEmpty() ) {
       Object id = serviceProperties.get( SERVICE_PROPERTY_PLUGIN_ID );
       if ( id != null && id instanceof String ) {
-        removePluginSystemAccess( (String) id, readAccess );
+        removeReadOnlyPluginSystemAccess( (String) id, readAccess );
+      } else {
+        Object isUserContent = serviceProperties.get( SERVICE_PROPERTY_IS_USER_CONTENT );
+        if ( isUserContent != null && isUserContent instanceof Boolean && ((Boolean) isUserContent).booleanValue() ) {
+          userContentReadAccesses.remove( readAccess );
+        }
       }
     }
   }
@@ -126,60 +169,68 @@ public final class ContentAccessFactory implements IContentAccessFactoryExtended
     this.userContentAccess = null;
   }
 
-  public ContentAccessFactory( String parentPluginId ) {
-    this.volumePath = System.getProperty( "java.io.tmpdir" );
-    this.parentPluginId = parentPluginId;
+  public FileSystem getPluginStorageFilesystem() {
+    return storageFilesystem;
   }
 
-  public ContentAccessFactory( String volumePath, String parentPluginId ) {
-    this.volumePath = volumePath;
-    this.parentPluginId = parentPluginId;
+  public void setPluginStorageFilesystem( FileSystem storageFilesystem ) {
+    this.storageFilesystem = storageFilesystem;
+  }
+
+  public String getBaseStoragePath() {
+    return baseStoragePath;
+  }
+
+  public void setBaseStoragePath( String baseStoragePath ) {
+    this.baseStoragePath = baseStoragePath;
   }
 
   @Override
   public IUserContentAccessExtended getUserContentAccess( String basePath ) {
     if ( userContentAccess == null ) {
-      return new UserContentAccess( new ReadAccessProxy( userContentReadAccesses, basePath ) );
-    } else {
       if ( userContentReadAccesses.isEmpty() ) {
-        return userContentAccess;
+        return null;
       } else {
-        return new OverlayUserContentAccess( basePath, userContentAccess, userContentReadAccesses );
+        return new UserContentAccess( new ReadAccessProxy( userContentReadAccesses, basePath ) );
       }
+    } else {
+      return new OverlayUserContentAccess( basePath, userContentAccess, userContentReadAccesses );
     }
   }
 
   @Override
   public IReadAccess getPluginRepositoryReader( String basePath ) {
-    logger.info( "RO FileSystemOverlay for: " + basePath );
+    logger.info( "[PluginRepos]  RO FileSystemOverlay for: " + basePath );
     return getPluginRepositoryOverlay( basePath );
   }
 
   @Override
   public IRWAccess getPluginRepositoryWriter( String basePath ) {
-    logger.info( "RO FileSystemOverlay for: " + basePath );
+    logger.info( "[PluginRepos]  RW FileSystemOverlay for: " + basePath );
     return getPluginRepositoryOverlay( basePath );
   }
 
   @Override
   public IReadAccess getPluginSystemReader( String basePath ) {
+    logger.info( "[PluginSystem] RO FileSystemOverlay for <" + parentPluginId + ">: " + basePath );
     return getOtherPluginSystemReader( parentPluginId, basePath );
   }
 
   @Override
   public IRWAccess getPluginSystemWriter( String basePath ) {
+    logger.info( "[PluginSystem] RW FileSystemOverlay for <" + parentPluginId + ">: " + basePath );
     return getOtherPluginSystemWriter( parentPluginId, basePath );
   }
 
   @Override
   public IReadAccess getOtherPluginSystemReader( String pluginId, String basePath ) {
-    logger.info( ( pluginId.equals( parentPluginId ) ? "[SELF]" : "[OTHER]" ) + " RO FileSystemOverlay for <" + pluginId + ">: " + basePath );
+    logger.info( "[PluginOtherSystem]  RO FileSystemOverlay for <" + pluginId + ">: " + basePath );
     return getPluginSystemOverlay( pluginId, basePath );
   }
 
   @Override
   public IRWAccess getOtherPluginSystemWriter( String pluginId, String basePath ) {
-    logger.info( ( pluginId.equals( parentPluginId ) ? "[SELF]" : "[OTHER]" ) + " RO FileSystemOverlay for <" + pluginId + ">: " + basePath );
+    logger.info( "[PluginOtherSystem]  RW FileSystemOverlay for <" + pluginId + ">: " + basePath );
     return getPluginSystemOverlay( pluginId, basePath );
   }
 
@@ -201,8 +252,8 @@ public final class ContentAccessFactory implements IContentAccessFactoryExtended
   }
 
   private String createStoragePath( String namespace, String id ) {
-    // TODO: validate that basePath does not cross back the namespace boundary
-    Path storagePath =  id != null ? storageFilesystem.getPath( volumePath, namespace, id ) : storageFilesystem.getPath( volumePath, namespace );
+    // TODO: validate that basePath does not travel across the namespace boundary
+    Path storagePath =  id != null ? storageFilesystem.getPath( baseStoragePath, namespace, id ) : storageFilesystem.getPath( baseStoragePath, namespace );
     File storage = storagePath.toFile();
     if ( storage.exists() && !storage.isDirectory() ) {
       throw new IllegalStateException( "Expected path to be a directory: " + storagePath.toString() );
@@ -213,11 +264,4 @@ public final class ContentAccessFactory implements IContentAccessFactoryExtended
     return storagePath.toString();
   }
 
-  public FileSystem getPluginStorageFilesystem() {
-    return storageFilesystem;
-  }
-
-  public void setPluginStorageFilesystem( FileSystem storageFilesystem ) {
-    this.storageFilesystem = storageFilesystem;
-  }
 }
